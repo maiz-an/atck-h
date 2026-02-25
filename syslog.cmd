fa39a9aadc346abbec8f9f8638d69fdbab001b86
Maizan <memaizanx@gmail.com>
2026-02-26 02:25:45 +0300

    delay number -q

--- syslog.cmd
+++ syslog.cmd
@@ -3,7 +3,7 @@ title SYSTEM BREACHED 3.13+1
 color 0a
 
 for /l %%i in (1,1,2) do (
-    start "SECURITY ALERT %%i" cmd /c "color 4 && title SYSTEM BREACH && echo. && echo [!] Unauthorized Access Detected... && echo. && echo Hacking Laptop... %%i%% && echo. && echo Accessing Camera... && echo Accessing Files... && echo Encrypting Data... && echo. && echo System Breached..! && echo. && pause > nul"
+    start "SECURITY ALERT %%i" cmd /c "color 4 && title SYSTEM BREACH 3.15 && echo. && echo [!] Unauthorized Access Detected... && echo. && echo Hacking Laptop... %%i%% && echo. && echo Accessing Camera... && echo Accessing Files... && echo Encrypting Data... && echo. && echo System Breached..! && echo. && pause > nul"
 )
 
 timeout /t 2 > nul
