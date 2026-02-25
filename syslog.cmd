3544fe0fde2bd7f71913bd5c74be07932d456b3d
Maizan <memaizanx@gmail.com>
2026-02-25 16:27:37 +0300

    atck

--- syslog.cmd
+++ syslog.cmd
@@ -2,7 +2,7 @@
 title SYSTEM BREACH DETECTED
 color 0a
 
-for /l %%i in (1,1,5) do (
+for /l %%i in (1,1,10) do (
     start "SECURITY ALERT %%i" cmd /c "color 4 && title CRITICAL ERROR %%i && echo. && echo [!] Unauthorized Access Detected... && echo. && echo Hacking Laptop... %%i%% && echo. && echo Accessing Camera... && echo Accessing Files... && echo Encrypting Data... && echo. && echo DO NOT TURN OFF YOUR COMPUTER! && echo. && echo Press any key to attempt recovery... && pause > nul"
 )
 
