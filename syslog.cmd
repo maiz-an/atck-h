5f21636a0732cfaf1ceeaa38ac28c95923fab858
Maizan <memaizanx@gmail.com>
2026-02-26 02:08:54 +0300

    dela number

--- syslog.cmd
+++ syslog.cmd
@@ -1,8 +1,8 @@
 @echo off
-title SYSTEM BREACHED
+title SYSTEM BREACHED 3.12+1
 color 0a
 
-for /l %%i in (1,1,7) do (
+for /l %%i in (1,1,2) do (
     start "SECURITY ALERT %%i" cmd /c "color 4 && title SYSTEM BREACH && echo. && echo [!] Unauthorized Access Detected... && echo. && echo Hacking Laptop... %%i%% && echo. && echo Accessing Camera... && echo Accessing Files... && echo Encrypting Data... && echo. && echo System Breached..! && echo. && pause > nul"
 )
 
