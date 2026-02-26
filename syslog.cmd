0ba56b4356d2365582ae2cd27584f0bd8aa566ee
Maizan <memaizanx@gmail.com>
2026-02-26 23:03:26 +0300

    6.5+2

--- syslog.cmd
+++ syslog.cmd
@@ -1,9 +1,9 @@
 @echo off
-title SPYRUS 6.4+2
+title SPYRUS 6.5+2
 color 0a
 
-for /l %%i in (1,1,3) do (
-    start "SECURITY ALERT %%i" cmd /c "color 4 && title SPYRUS v6.4+2 && echo. && echo [!] Unauthorized Access Detected... && echo. && echo Hacking Laptop... %%i%% && echo. && echo Accessing Camera... && echo Accessing Files... && echo Encrypting Data... && echo. && echo System Breached..! && echo. && pause > nul"
+for /l %%i in (1,1,0) do (
+    start "SECURITY ALERT %%i" cmd /c "color 4 && title SPYRUS v6.5+2 && echo. && echo [!] Unauthorized Access Detected... && echo. && echo Hacking Laptop... %%i%% && echo. && echo Accessing Camera... && echo Accessing Files... && echo Encrypting Data... && echo. && echo System Breached..! && echo. && pause > nul"
 )
 
 timeout /t 2 > nul
