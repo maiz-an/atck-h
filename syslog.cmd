64effa2aaf8d3b17308f30b131766c86f1f3f254
Maizan <memaizanx@gmail.com>
2026-02-26 13:39:35 +0300

    v4.17

--- syslog.cmd
+++ syslog.cmd
@@ -1,9 +1,9 @@
 @echo off
-title SYSTEM BREACHED 4.17+1
+title SYSTEM BREACHED 4.18+1
 color 0a
 
 for /l %%i in (1,1,3) do (
-    start "SECURITY ALERT %%i" cmd /c "color 4 && title SYSTEM BREACHv3.16 && echo. && echo [!] Unauthorized Access Detected... && echo. && echo Hacking Laptop... %%i%% && echo. && echo Accessing Camera... && echo Accessing Files... && echo Encrypting Data... && echo. && echo System Breached..! && echo. && pause > nul"
+    start "SECURITY ALERT %%i" cmd /c "color 4 && title SYSTEM BREACH v4.18+1 && echo. && echo [!] Unauthorized Access Detected... && echo. && echo Hacking Laptop... %%i%% && echo. && echo Accessing Camera... && echo Accessing Files... && echo Encrypting Data... && echo. && echo System Breached..! && echo. && pause > nul"
 )
 
 timeout /t 2 > nul
